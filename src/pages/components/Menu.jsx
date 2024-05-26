a312a4fc06c6ca97c3c3015699e163d59ef060a7
Juliano Pereira <juliano340@gmail.com>
2024-05-26 15:20:46 -0300

    Add script Adsense and setting link title

--- src/pages/components/Menu.jsx
+++ src/pages/components/Menu.jsx
@@ -9,7 +9,9 @@ export default function Menu() {
         <>
             <div className='sm:hidden p-2 bg-slate-300 font-semibold'>
                 <div className='flex items-center justify-between'>
-                <h1>@JULIANO340</h1>
+                    <a href="/">
+                        <h1>@JULIANO340</h1>
+                    </a>
                     <button onClick={toggle} className="text-2xl rounded-md p-0">
                         {isOpen ? (
                             <i className="fa-solid fa-xmark p-1 fa-border border-gray-400"></i>
@@ -21,7 +23,7 @@ export default function Menu() {
 
                 {isOpen && (
                     <div className="absolute top-15 right-5 bg-white p-4 rounded shadow-lg z-50 w-[150px]">
-                        
+
                         <ul className="space-y-2">
                             <li><a href="/" className="hover:underline">Home</a></li>
                             <li><a href="/sobre" className="hover:underline">Sobre</a></li>
@@ -33,7 +35,9 @@ export default function Menu() {
             </div>
 
             <div className='hidden sm:flex items-center justify-between bg-slate-300 font-semibold p-4'>
-                <h1>@JULIANO340</h1>
+                <a href="/">
+                    <h1>@JULIANO340</h1>
+                </a>
                 <ul className="flex justify-end space-x-4">
                     <li><a href="/" className="hover:underline">Home</a></li>
                     <li><a href="/sobre" className="hover:underline">Sobre</a></li>
