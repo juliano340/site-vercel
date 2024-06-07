35465a8119f30a89bcaca64a4c5c2344e174e03d
Juliano Pereira <juliano340@gmail.com>
2024-06-07 00:33:20 -0300

    Hero title adjust

--- src/pages/components/HeroSection.js
+++ src/pages/components/HeroSection.js
@@ -12,15 +12,17 @@ const HeroSection = () => {
         height={150}
         className="rounded-full mb-4"
       />
-      <h1 className="text-5xl text-center font-bold mb-4">Olá, eu sou um Programador Web</h1>
-      <TypingEffect
-        text={["Desenvolvedor Front-end", "Desenvolvedor Back-end", "Desenvolvedor Full Stack"]}
-        speed={100}
-        eraseSpeed={50}
-        eraseDelay={2000}
-        typingDelay={500}
-        className="text-2xl"
-      />
+      <h1 className="text-5xl text-center font-bold mb-4">Olá, eu sou um Programador Web!</h1>
+      <div className="text-2xl text-center">
+        <TypingEffect
+          text={["Desenvolvedor Front-end", "Desenvolvedor Back-end", "Desenvolvedor Full Stack!"]}
+          speed={100}
+          eraseSpeed={50}
+          eraseDelay={2000}
+          typingDelay={500}
+          className=""
+        />
+      </div>
       <p className="mt-6 text-lg text-center text-gray-400">Criando soluções web eficientes e inovadoras.</p>
       <Link href="/contato" passHref>
         <div className="mt-8 px-6 py-3 bg-blue-500 text-white rounded-full hover:bg-blue-700 cursor-pointer transition duration-300 ease-in-out">
