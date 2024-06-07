33b92fa9dc244865dcf70a2a61a5689181065f58
Juliano Pereira <juliano340@gmail.com>
2024-06-07 10:51:20 -0300

    Ajustando title e favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,11 +11,13 @@ class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <meta charSet="UTF-8" />
+          <link rel="icon" href="/favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
           <script src="https://kit.fontawesome.com/0934ea43d0.js" crossOrigin="anonymous"></script>
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8578901708699710" crossOrigin="anonymous"></script>
+          <title>@JULIANO340</title>
         </Head>
         <body>
           <Main />
