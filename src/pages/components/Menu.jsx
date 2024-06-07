dee8a2646947cd40be6e54eae50504db67bc45b4
Juliano Pereira <juliano340@gmail.com>
2024-06-07 02:02:27 -0300

    Ajuste link #about

--- src/pages/components/Menu.jsx
+++ src/pages/components/Menu.jsx
@@ -32,7 +32,7 @@ export default function Menu() {
                             <li><a href="/" className="block hover:underline">HOME</a></li>
                             <li><a href="#about" className="block hover:underline">SOBRE</a></li>
                             <li><a href="/blog" className="block hover:underline">BLOG</a></li>
-                            <li><a href="/contato" className="block hover:underline">CONTATO</a></li>
+                            <li><a href="/http://localhost:3000/contato" className="block hover:underline">CONTATO</a></li>
                         </ul>
                     </div>
                 )}
@@ -44,7 +44,7 @@ export default function Menu() {
                 </a>
                 <ul className="flex space-x-4 text-gray-700">
                     <li><a href="/" className="hover:underline">HOME</a></li>
-                    <li><a href="#about" className="hover:underline">SOBRE</a></li>
+                    <li><a href="/#about" className="hover:underline">SOBRE</a></li>
                     <li><a href="/blog" className="hover:underline">BLOG</a></li>
                     <li><a href="contato" className="hover:underline">CONTATO</a></li>
                 </ul>
