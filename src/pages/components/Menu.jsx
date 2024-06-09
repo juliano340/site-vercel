c278467ee603cd479eff3a87a6aa13b3bd9ca9ae
Juliano Pereira <juliano340@gmail.com>
2024-06-09 00:15:10 -0300

    Ajuste padding menu large

--- src/pages/components/Menu.jsx
+++ src/pages/components/Menu.jsx
@@ -7,49 +7,52 @@ export default function Menu() {
 
     return (
         <>
-            <div className="fixed top-0 left-0 right-0 sm:hidden p-4 bg-slate-300 font-semibold z-50">
-                <div className="flex items-center justify-between">
+            <div className="fixed w-full top-0 left-0 bg-slate-300 font-semibold z-50">
+                <div className="sm:hidden p-4">
+                    <div className="flex items-center justify-between">
+                        <a href="/" className="text-xl font-bold">
+                            @JULIANO340
+                        </a>
+                        <button
+                            onClick={toggle}
+                            className="text-2xl rounded-md p-2 bg-gray-200 hover:bg-gray-300 transition"
+                            aria-expanded={isOpen}
+                            aria-label="Toggle menu"
+                        >
+                            {isOpen ? (
+                                <i className="fa-solid fa-xmark" aria-hidden="true"></i>
+                            ) : (
+                                <i className="fa-solid fa-bars" aria-hidden="true"></i>
+                            )}
+                        </button>
+                    </div>
+
+                    {isOpen && (
+                        <div className="absolute top-16 right-5 bg-white  rounded shadow-lg z-50 w-[150px]">
+                            <ul className="space-y-2 text-gray-700">
+                                <li><a href="/" className="block hover:underline">HOME</a></li>
+                                <li><a href="/#about" className="block hover:underline">SOBRE</a></li>
+                                <li><a href="/blog" className="block hover:underline">BLOG</a></li>
+                                <li><a href="/contato" className="block hover:underline">CONTATO</a></li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
+
+                <div className="hidden sm:flex items-center justify-between p-6">
                     <a href="/" className="text-xl font-bold">
                         @JULIANO340
                     </a>
-                    <button
-                        onClick={toggle}
-                        className="text-2xl rounded-md p-2 bg-gray-200 hover:bg-gray-300 transition"
-                        aria-expanded={isOpen}
-                        aria-label="Toggle menu"
-                    >
-                        {isOpen ? (
-                            <i className="fa-solid fa-xmark" aria-hidden="true"></i>
-                        ) : (
-                            <i className="fa-solid fa-bars" aria-hidden="true"></i>
-                        )}
-                    </button>
+                    <ul className="flex space-x-4 text-gray-700">
+                        <li><a href="/" className="hover:underline">HOME</a></li>
+                        <li><a href="/#about" className="hover:underline">SOBRE</a></li>
+                        <li><a href="/blog" className="hover:underline">BLOG</a></li>
+                        <li><a href="/contato" className="hover:underline">CONTATO</a></li>
+                    </ul>
                 </div>
-
-                {isOpen && (
-                    <div className="absolute top-16 right-5 bg-white p-4 rounded shadow-lg z-50 w-[150px]">
-                        <ul className="space-y-2 text-gray-700">
-                            <li><a href="/" className="block hover:underline">HOME</a></li>
-                            <li><a href="/#about" className="block hover:underline">SOBRE</a></li>
-                            <li><a href="/blog" className="block hover:underline">BLOG</a></li>
-                            <li><a href="/contato" className="block hover:underline">CONTATO</a></li>
-                        </ul>
-                    </div>
-                )}
             </div>
 
-            <div className="fixed top-0 left-0 right-0 hidden sm:flex items-center justify-between bg-slate-300 font-semibold p-4 z-50">
-                <a href="/" className="text-xl font-bold">
-                    @JULIANO340
-                </a>
-                <ul className="flex space-x-4 text-gray-700">
-                    <li><a href="/" className="hover:underline">HOME</a></li>
-                    <li><a href="/#about" className="hover:underline">SOBRE</a></li>
-                    <li><a href="/blog" className="hover:underline">BLOG</a></li>
-                    <li><a href="/contato" className="hover:underline">CONTATO</a></li>
-                </ul>
-            </div>
-            <div className="pt-16 sm:pt-20"></div> {/* Spacing for fixed nav */}
+            <div className="mt-16"></div> {/* Ajuste a margem superior conforme necessário */}
         </>
     );
 }
