ed819b92603a212daf06fb8c63f213c2fde14de6
Juliano Pereira <juliano340@gmail.com>
2024-06-09 23:51:50 -0300

    Adicionando tag title no _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,16 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import Layout from './components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
+
+
     <Layout>
+      <Head>
+
+        <title>@JULIANO340</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
