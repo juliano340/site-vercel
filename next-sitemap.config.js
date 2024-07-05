f710ed58991fa1de9553fa6ae94247c22c18c607
Juliano Pereira <juliano340@gmail.com>
2024-07-04 23:51:57 -0300

    siteUrl sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https ://localhost:3000', // Substitua pelo URL do seu site
+    siteUrl: 'https://juliano340.com', // Substitua pelo URL do seu site
     generateRobotsTxt: true, // Gera o arquivo robots.txt
   };
   
