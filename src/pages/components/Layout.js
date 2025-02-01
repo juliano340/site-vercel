99007aacf78337fbacaf8eebbec08d6a77cb8732
Juliano Pereira <juliano340@gmail.com>
2025-01-31 23:23:20 -0300

    FEAT(app): Adicioa page "links" sem template.

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Menu from './Menu';
 import Footer from './Footer';
+import { useRouter } from 'next/router';
+import LinksPage from '../links'; // Certifique-se de importar o LinksPage corretamente
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
+  if (router.pathname === '/links') {
+    return (
+      <>
+        {children}
+      </>
+    );
+  }
+
   return (
     <>
       <Menu />
       {children}
-      
       <Footer />
     </>
   );
---
+++ src/pages/links/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './LinkPages.module.css';
+
+const LinksPage = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Links Page</h1>
+            <p className={styles.description}>Welcome to the Links Page!</p>
+        </div>
+    );
+};
+
+export default LinksPage;
